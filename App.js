6b0b5461aaa691780befb231b98e558c3d86ddaa
Stephanie Viveros <sviveros05@gmail.com>
2025-08-29 17:39:11 -0500

    Add background gradient

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View>
+    <LinearGradient
+      style={styles.rootContainer}
+      colors={["#ddb52f", "#3b021f"]}
+    >
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {},
+  rootContainer: {
+    backgroundColor: "#ddb52f",
+    flex: 1,
+  },
 });
