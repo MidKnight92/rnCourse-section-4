2677fa6a7f815f763aa61cda071fda13277b8fe0
Stephanie Viveros <sviveros05@gmail.com>
2025-08-29 12:31:58 -0500

    Add initial StartGameScreen content

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import { StyleSheet, View, FlatList, Button } from "react-native";
-
+import { StyleSheet, View } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
- 
   return (
     <View style={styles.appContainer}>
-      
+      <StartGameScreen />
     </View>
   );
 }
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,3 +1,11 @@
-export default function StartGameScreen(){
-    return 'start'
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
