99e17bc63b4f8e4d25b7d65b28d3bb1e9d2e262e
Stephanie Viveros <sviveros05@gmail.com>
2025-09-02 18:20:23 -0500

    Add platform specific style to title border

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,36 +1,57 @@
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import {
+  useWindowDimensions,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
 import Title from "../components/ui/Title";
 import { Colors } from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
+import { useMemo } from "react";
 
 export default function GameOverScreen({ rounds, choosenNumber, onGameReset }) {
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+
+  const imgSize = useMemo(
+    () => (windowWidth < 380 ? 150 : windowHeight < 400 ? 80 : 300),
+    height
+  );
+
   return (
-    <View style={styles.container}>
-      <Title>Game Over</Title>
-      <View style={styles.imgContainer}>
-        <Image
-          source={require("../assets/images/success.png")}
-          style={styles.img}
-        />
+    <ScrollView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Title>Game Over</Title>
+        <View
+          style={[
+            styles.imgContainer,
+            { width: imgSize, height: imgSize, borderRadius: imgSize / 2 },
+          ]}
+        >
+          <Image
+            source={require("../assets/images/success.png")}
+            style={styles.img}
+          />
+        </View>
+        <Text style={styles.summaryText}>
+          <Text style={styles.highlight}>{rounds.length}</Text> rounds were
+          needed to guess your number{" "}
+          <Text style={styles.highlight}>{choosenNumber}</Text>.
+        </Text>
+        <PrimaryButton btnHandler={onGameReset}>Start New Game</PrimaryButton>
       </View>
-      <Text style={styles.summaryText}>
-        <Text style={styles.highlight}>{rounds.length}</Text> rounds were needed
-        to guess your number{" "}
-        <Text style={styles.highlight}>{choosenNumber}</Text>.
-      </Text>
-      <PrimaryButton btnHandler={onGameReset}>Start New Game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 }
-const { width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   imgContainer: {
-    borderRadius: width < 380 ? 150 : 300,
+    // borderRadius: width < 380 ? 150 : 300,
     borderWidth: 3,
     borderColor: Colors.primary700,
-    width: width < 380 ? 150 : 300,
-    height: width < 380 ? 150 : 300,
+    // width: width < 380 ? 150 : 300,
+    // height: width < 380 ? 150 : 300,
     overflow: "hidden",
     margin: 36,
   },
